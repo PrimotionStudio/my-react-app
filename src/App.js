4e0bd9e45a6e6610238a80084348994c3c5c5763
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-14 16:30:42 +0100

    "Deleted README.md, logo.svg, App.test.js, reportWebVitals.js, and setupTests.js files. Modified public/index.html, src/App.js, and src/index.js files."

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('Prime');
+	const [events, setEvents] = useState([
+		{
+			id: 1,
+			title: 'Birthday',
+			description: 'Lorem ipsum dolor sit amet.',
+		},
+		{ id: 2, title: 'Wedding', description: 'Lorem ipsum dolor sit amet.' },
+		{ id: 3, title: 'Burial', description: 'Lorem ipsum dolor sit amet.' },
+	]);
+
+	const changeName = (e) => {
+		const newName = prompt("What's your new name", name);
+		setName(newName);
+	};
+
+	const deleteEvent = (eventId) => {
+		const remainingEvents = events.filter((event) => event.id !== eventId);
+		setEvents(remainingEvents);
+	};
+	return (
+		<div className='App'>
+			<h1>My name is {name}</h1>
+			<button onClick={changeName}>Change Name</button>
+
+			{events.map((event, index) => (
+				<div key={event.id}>
+					<h3>
+						{index} - {event.title}
+					</h3>
+					<p>{event.description}</p>
+					<button onClick={() => deleteEvent(event.id)}>
+						Delete Event
+					</button>
+					<hr></hr>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
